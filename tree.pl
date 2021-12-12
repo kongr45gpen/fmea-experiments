e80a3bddd8e07927581002943f009e42f0309a0f
alezakos <electrovesta@gmail.com>
2021-12-12 22:37:57 +0200

    Add same implementation using trees

---
+++ tree.pl
@@ -0,0 +1,23 @@
+:- use_rendering(svgtree, [list(false)]).
+
+pind(event1, 0.51).
+pind(event2, 0.3).
+
+p(A, P) :- pind(A, P).
+
+p(and(A, A) , P) :-
+    p(A, P).
+
+p(and(A, B) , P) :-
+    A \= B,
+    pind(A, P1),
+    pind(B, P2),
+    P is P1 * P2.
+
+p(or(A, A) , P) :- pind(A, P).
+
+p(or(A, B) , P) :-
+    A \= B,
+    pind(A, P1),
+    pind(B, P2),
+    P is 1 - (1 - P1) * (1 - P2).
